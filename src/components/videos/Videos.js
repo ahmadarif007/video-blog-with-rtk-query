079248786c15287835d52c5f0c0e3e4832086497
Ahmad Arif <ahmadarif.0007@gmail.com>
2022-10-08 12:42:18 +0600

    Arif | v.1 | Getting videos on home page

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,24 @@
+import { useGetVideosQuery } from "../../features/api/apiSlice";
 import Video from "./Video";
+import Loading from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+    const { data: videos, isError, isLoading, isSuccess } = useGetVideosQuery();
+
+    let content = null;
+    if (isLoading) {
+        content = <Loading />;
+    }
+    if (!isLoading && isError) {
+        content = <Error message="There was an error!" />;
+    }
+    if (!isLoading && !isError && videos.length === 0) {
+        content = <Error message="No video found!" />;
+    }
+    if (!isLoading && !isError && videos.length > 0) {
+        content = videos.map((video) => <Video key={video.id} video={video} />);
+    }
+
+    return <>{content}</>;
 }
