e00cd65f99a57d773dedf5abc61764faa19483b1
Ahmad Arif <ahmadarif.0007@gmail.com>
2022-10-08 13:20:35 +0600

    Arif | v.1 | Getting Single video from server API

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -1,15 +1,40 @@
+import { useParams } from "react-router-dom";
+import { useGetVideoQuery } from "../../features/api/apiSlice";
 import Description from "../video/Description";
 import Player from "../video/Player";
 import RelatedVideos from "../video/related/RelatedVideos";
+import Loading from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
 
 export default function Video() {
+    const { videoId } = useParams();
+    const { data: video, isLoading, isError } = useGetVideoQuery(videoId);
+
+    let content = null;
+    if (isLoading) {
+        content = <Loading />;
+    }
+    if (!isLoading && isError) {
+        content = <Error message="There was an error!" />;
+    }
+    if (!isLoading && !isError && !video.id) {
+        content = <Error message="No video found!" />;
+    }
+    if (!isLoading && !isError && video.id) {
+        content = (
+            <>
+                <Player title={video.title} link={video.link} />
+                <Description video={video} />
+            </>
+        );
+    }
+
     return (
         <section className="pt-6 pb-20 min-h-[calc(100vh_-_157px)]">
             <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
                 <div className="grid grid-cols-3 gap-2 lg:gap-8">
                     <div className="col-span-full w-full space-y-8 lg:col-span-2">
-                        <Player />
-                        <Description />
+                        {content}
                     </div>
 
                     <RelatedVideos />
